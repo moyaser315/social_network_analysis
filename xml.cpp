d7ece7a9998e25556620b8f60b0edc11c9bf706f
Mahmoud Abuelnaga <abuelnaga.m0@gmail.com>
2023-12-05 01:13:43 +0200

    Separate XmlParser and XmlTree class and allowed the Xml tree to be printed

--- xml.cpp
+++ xml.cpp
@@ -6,18 +6,6 @@
 #include <queue>
 using namespace std;
 
-
-string strip_line(const string &line) {
-    size_t first_non_space = line.find_first_not_of(" \t\n\r");
-    size_t last_non_space = line.find_last_not_of(" \t\n\r");
-
-    if (first_non_space == string::npos) {
-        return "";
-    }
-
-    return line.substr(first_non_space, last_non_space - first_non_space + 1);
-}
-
 struct Node
 {
 public:
@@ -32,25 +20,105 @@ public:
             throw runtime_error("Node must have a name.");
         }
     }
+
+    string to_string(const bool &prettify = true, const int &pre_spaces = 0)
+    {
+        string res{""};
+        if (prettify)
+        {
+            for (int i = 0; i < pre_spaces; i++)
+            {
+                res += " ";
+            }
+        }
+
+        if (name != "__TEXT__")
+        {
+
+            res += "<" + name + ">";
+            if (prettify)
+            {
+                res += "\n";
+            }
+
+            for (const auto child : children)
+            {
+                res += child->to_string(prettify, pre_spaces + 4);
+            }
+
+            if (prettify)
+            {
+                res += "\n";
+                for (int i = 0; i < pre_spaces; i++)
+                {
+                    res += " ";
+                }
+            }
+
+            res += "</" + name + ">";
+            if (prettify) {
+                res += "\n";
+            }
+        }
+        else
+        {
+            res += value;
+        }
+
+        return res;
+    }
 };
 
-class XML
+class XmlParser
 {
 private:
-    Node *root{nullptr};
+    static bool is_opening_tag(const string &token)
+    {
+        return regex_match(token, regex("<[a-zA-Z]+>"));
+    }
 
-    vector<string> read_file(const string &file_path)
+    static bool is_tag(const string &token)
+    {
+        return regex_match(token, regex("<[a-zA-Z]+>|</[a-zA-Z]+>"));
+    }
+
+    static bool is_closing_tag(const string &token)
+    {
+        return regex_match(token, regex("</[a-zA-Z]+>"));
+    }
+
+    static bool is_text_node(const string &token)
+    {
+        return regex_match(token, regex("[_a-zA-Z0-9.,?&\"'\\s]+"));
+    }
+
+    static string strip_line(const string &line)
+    {
+        size_t first_non_space = line.find_first_not_of(" \t\n\r");
+        size_t last_non_space = line.find_last_not_of(" \t\n\r");
+
+        if (first_non_space == string::npos)
+        {
+            return "";
+        }
+
+        return line.substr(first_non_space, last_non_space - first_non_space + 1);
+    }
+
+    static vector<string> read_file(const string &xml_file_path)
     {
         // open file
-        ifstream file(file_path);
-        if (!file.is_open()) {  // if error opening file
+        ifstream file(xml_file_path);
+        if (!file.is_open())
+        { // if error opening file
             throw runtime_error("FileSystem Error: Unable to read the file");
         }
 
         // read lines
-        string txt {""};
+        string txt{""};
         string line;
-        while (getline(file, line)) {
+        while (getline(file, line))
+        {
             txt += strip_line(line) + " ";
         }
 
@@ -62,113 +130,145 @@ private:
 
         istringstream txt_stream(txt);
         vector<string> tokens;
-        while (getline(txt_stream, line)) {
+        while (getline(txt_stream, line))
+        {
             tokens.push_back(line);
         }
-        
+
         return tokens;
     }
 
-    bool is_opening_tag(const string &token) {
-        return regex_match(token, regex("<[a-zA-Z]+>"));
+    static void clear(Node *node)
+    {
+        if (!node)
+        {
+            return;
+        }
+
+        for (const auto n : node->children)
+        {
+            clear(n);
+        }
+
+        delete node;
     }
 
-    bool is_tag(const string &token) {
-        return regex_match(token, regex("<[a-zA-Z]+>|</[a-zA-Z]+>"));
-    }
-
-    bool is_closing_tag(const string &token) {
-        return regex_match(token, regex("</[a-zA-Z]+>"));
-    }
-
-    bool is_text_node(const string &token) {
-        return regex_match(token, regex("[_a-zA-Z0-9.,?&\"'\\s]+"));
-    }
-
-    pair<Node *, int> create_node(const vector<string> &tokens, const int &token_idx) {
-        if (token_idx >= (int)tokens.size()) {
+    static pair<Node *, int> create_node(const vector<string> &tokens, const int &token_idx)
+    {
+        if (token_idx >= (int)tokens.size())
+        {
             return make_pair(nullptr, -1);
         }
 
         string token = tokens[token_idx];
 
-        if (is_text_node(token)) {
-            Node *node {new Node{"TEXT", token}};
+        if (is_text_node(token))
+        {
+            Node *node{new Node{"__TEXT__", token}};
             return make_pair(node, token_idx + 1);
         }
 
-        if (is_opening_tag(token)) {
-            string node_name {token.substr(1, (int)token.size() - 2)};
-            Node *node {new Node{node_name}};
+        if (is_opening_tag(token))
+        {
+            string node_name{token.substr(1, (int)token.size() - 2)};
+            Node *node{new Node{node_name}};
 
-            int next_token_idx {token_idx + 1};
-            while (is_opening_tag(tokens[next_token_idx]) || is_text_node(tokens[next_token_idx])) {
-                pair<Node *, int> res {create_node(tokens, next_token_idx)};
+            int next_token_idx{token_idx + 1};
+            while (is_opening_tag(tokens[next_token_idx]) || is_text_node(tokens[next_token_idx]))
+            {
+                pair<Node *, int> res;
+                try
+                {
+                    res = create_node(tokens, next_token_idx);
+                }
+                catch (const exception &e)
+                {
+                    clear(node);
+                    throw e;
+                }
+
                 node->children.push_back(res.first);
                 next_token_idx = res.second;
             }
-            
-            if (is_closing_tag(tokens[next_token_idx])) {
-                string closing_node {tokens[next_token_idx].substr(2, (int)tokens[next_token_idx].size() - 3) };
-                if (node_name == closing_node) {
+
+            if (is_closing_tag(tokens[next_token_idx]))
+            {
+                string closing_node{tokens[next_token_idx].substr(2, (int)tokens[next_token_idx].size() - 3)};
+                if (node_name == closing_node)
+                {
                     return make_pair(node, next_token_idx + 1);
-                } else {
+                }
+                else
+                {
+                    clear(node);
                     throw runtime_error("Parsing Error: Closing tag doesn't match.");
                 }
             }
-
-        } else {
-            throw runtime_error("Runtime Error: Reached a closing tag that shouldn't be there");
+            else
+            {
+                clear(node);
+                throw runtime_error("Parsing Error: Closing tag couldn't be found");
+            }
         }
-
+        else
+        {
+            throw runtime_error("Parsing Error: Reached a closing tag that shouldn't be there");
+        }
     }
-
-    
 
 public:
-    XML(const string &file_path)
+    static Node *parse(const string &file_path)
     {
-        vector<string> tokens {read_file(file_path)};
-        pair<Node*, int> res {create_node(tokens, 0)};
-        root = res.first;
+        if (file_path == "")
+        {
+            throw runtime_error("Input Error: must enter a path for a file");
+        }
+
+        vector<string> tokens{read_file(file_path)};
+
+        pair<Node *, int> res{create_node(tokens, 0)};
+        return res.first;
+    }
+};
+
+class XmlTree
+{
+private:
+    Node *root{nullptr};
+
+    void clear(Node *node)
+    {
+        if (!node)
+        {
+            return;
+        }
+
+        for (const auto n : node->children)
+        {
+            clear(n);
+        }
+
+        delete node;
     }
 
-    void print_by_level()
+public:
+    XmlTree(const string &file_path)
     {
-        queue<Node *> nodes;
-        nodes.push(root);
-        int level{0};
+        root = XmlParser::parse(file_path);
+    }
 
-        while (!nodes.empty())
-        {
-            cout << "Level " << level << ": ";
+    ~XmlTree()
+    {
+        clear(root);
+    }
 
-            int size{(int)nodes.size()};
-            for (int i = 0; i < size; i++)
-            {
-                Node *node{nodes.front()};
-                nodes.pop();
-
-                for (const auto n: node->children) {
-                    nodes.push(n);
-                }
-
-                cout << node->name;
-                if (node->value != "") {
-                    cout << ": " << node->value;
-                }
-
-                cout << endl;
-            }
-            level++;
-
-            cout << endl;
-        }
+    string to_string(const bool &prettify = true) {
+        return root->to_string(prettify);
     }
 };
 
 int main()
 {
-    XML xml{"./sample.xml"};
-    xml.print_by_level();
+    XmlTree xml{"./sample.xml"};
+    cout << xml.to_string(false) << endl;
 }
