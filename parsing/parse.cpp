c3549cef6e0c060fc4dc2434f5f75a43693c1dce
Mahmoud <mahmoud.elngar.2510@gmail.com>
2023-12-26 16:00:59 +0200

    Parsing version 2

--- parsing/parse.cpp
+++ parsing/parse.cpp
@@ -34,3 +34,81 @@ string toline(string fpath)
     return nonspace;
 }
 
+
+string get_tag(string line, int &st, bool &f)
+{
+    // f is for checking it's closing
+    int i = st, be = st + 1, cnt = 0;
+    f = line[i + 1] == '/' ? 1 : 0;
+    while (line[i] != '>')
+    {
+        i++, cnt++;
+    }
+
+    st += (cnt + 1);
+    // if it's a closing tag we need to ignore "/" as it satrts by </
+    // so we will substring from index 2 </x-->2 and end at len of word excluding /
+    // if it's opening tag we start from index 1 and end at len of word
+    return (f ? line.substr(be + 1, cnt - 2) : line.substr(be, cnt - 1));
+}
+
+string get_value(string line, int &st)
+{
+
+    int i = st, be = st, cnt = 0;
+    while (line[i] != '<')
+    {
+        i++, cnt++;
+    }
+    st += (cnt);
+
+    return line.substr(be, cnt);
+}
+
+void build_tree(string &xml, tree_mul<string> *&tag, int &st)
+{
+    int n = xml.length() - 1;
+    bool f = 0;
+    string tag_name, value;
+    // pushing the opening tag
+    if (tag == nullptr)
+    {
+        tag_name = get_tag(xml, st, f);
+        tag = new tree_mul<string>(tag_name);
+
+        build_tree(xml, tag, st);
+        return;
+    }
+    while (st < n - 1)
+    {
+        if (xml[st] == '<')
+        {
+            tag_name = get_tag(xml, st, f);
+            if (f)
+            {
+
+                // knows if ther's mismatch in closing tags
+                if (tag->get_data() != tag_name)
+                {
+                    errors++;
+                    
+                }
+                return;
+                
+            }
+            else
+            {
+        
+                tree_mul<string> *child = new tree_mul<string>(tag_name);
+                tag->insert(child);
+                build_tree(xml, child, st);
+            }
+        }
+        else
+        {
+            value = get_value(xml, st);
+            tag->insert(value);
+        }
+    }
+}
+
--- parsing/parse.h
+++ parsing/parse.h
@@ -1,14 +1,18 @@
 #ifndef parse_H
 #define parse_H
 #include <iostream>
+#include "../data_structres/tree.h"
 #include <regex>
 #include <fstream>
 
 using namespace std;
 
 
-string strip_line(const string &line);
-string toline(string fpath);
+string strip_line(const string &line);// to delete space
+string toline(string fpath);// return xml file as string
+string get_tag(string line,int &st,bool &f);// get tag name
+string get_value(string line,int &st);// get value of the tag like name or id
+void build_tree(string &xml, tree_mul<string> *&tag, int &st);// xml to parse ,tag for building tree , st to trace where i stopped parsing
 
-
+int errors;
 #endif
