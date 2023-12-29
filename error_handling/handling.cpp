1808b9a59f1a95be257cb3a7c5416ad99c1b698f
Mahmoud <mahmoud.elngar.2510@gmail.com>
2023-12-29 16:09:46 +0200

    Editing Handling files

--- error_handling/handling.cpp
+++ error_handling/handling.cpp
@@ -1,55 +1,57 @@
 #include "./handling.h"
 
+using namespace std;
+
+stack<string> tags;
+stack<string> err;
+vector<int> positions;
+
 /// @brief detects errors and correct missing and closing tags
 /// @param xml the string to parse for error detection
-void detect(string &xml)
-{
+void detect(string &xml) {
     string op, close;
     bool f = 0;
-    for (int i = 0; i < xml.size(); i++)
-    {
-        if (xml[i] == '<')
-        {
+    for (int i = 0; i < xml.size(); i++) {
+        if (xml[i] == '<') {
             // if f is raised it's a closing after a value
-            if (f)
-            {
-                if (xml[i + 1] != '/')
-                {
+            if (f) {
+                if (xml[i + 1] != '/') {
+                    int temp = get_freq(tags.top());
+                    if (freq[temp] > 0)
+                        freq[temp]--;
                     cout << "missing closing tag for : " << tags.top() << "\n";
                     cout << "will be fixed immeditly for file consictincy\n";
-                    xml = xml.substr(0, i) + "</" + tags.top() + '>' + xml.substr(i);
+                    xml = xml.substr(0, i) + "</" + tags.top() + '>' +
+                          xml.substr(i);
 
-                    i += tags.top().size() + 2;
+                    i += tags.top().size() + 3;
                     tags.pop();
                 }
                 f = 0;
             }
             // see if it's closing
-            if (xml[i + 1] == '/')
-            {
+            if (xml[i + 1] == '/') {
                 i++;
                 int st = i;
-                while (xml[++i] != '>')
-                {
+                while (xml[++i] != '>') {
                     close += xml[i];
                 }
                 i--;
-                // if the stack is empty there are no opening tags and that means there was a missing opening tag
-                if (tags.empty())
-                {
+                // if the stack is empty there are no opening tags and that
+                // means there was a missing opening tag
+                if (tags.empty()) {
                     cout << "missing opening tag for : " << close << "\n";
                     cout << "will be fixed immeditly for file consictincy\n";
-                    xml = xml.substr(0, st - 1) + '<' + close + '>' + xml.substr(st - 1);
+                    xml = xml.substr(0, st - 1) + '<' + close + '>' +
+                          xml.substr(st - 1);
                 }
 
-                else
-                {
+                else {
                     // if closing == opening dismiss
                     if (tags.top() == close)
                         tags.pop();
 
-                    else
-                    {
+                    else {
                         // see if closing or opening is missing or is it
                         // mismatch
                         int temp = get_freq(close);
@@ -74,16 +76,20 @@ void detect(string &xml)
                             tags.push(close);
                             freq[temp]++;
                         } else {
-                            //TODO waiting mismatch function
+                            cout << freq[temp] << "  ";
+                            cout << "closing tag mismatch for : " << tags.top()
+                                 << "\n";
+                            cout << close << "   " << tags.top() << "\n";
+                            err.push(tags.top());
+                            tags.pop();
+                            cout << close << "   " << tags.top() << "\n";
+                            positions.push_back(st);
                         }
                     }
                 }
-            }
-            else
-            {
+            } else {
                 // get the opening to push to the stack
-                while (xml[++i] != '>')
-                {
+                while (xml[++i] != '>') {
                     op += xml[i];
                 }
                 tags.push(op);
@@ -95,23 +101,32 @@ void detect(string &xml)
             close = "";
         }
         // if it's text ignore but raise a flag that it needs closing
-        else if (xml[i] == '>')
-        {
-            if (xml[i + 1] == '<')
-            {
+        else if (xml[i] == '>') {
+            if (xml[i + 1] == '<') {
                 f = 0;
-            }
-            else
-            {
+            } else {
                 f = 1;
             }
-        }
-        else
+        } else
             continue;
     }
     bool e = 0;
+    // output if there are still missing tags we couldn't handle and give a
+    // warning that may have caused mismatch
+    for (int i = 0; i < 11; i++) {
+        if (freq[i] < 0) {
+            e = 1;
+            cout << "there was a missing opening tag for : " << IDs[i] << "\n";
+        }
+        if (freq[i] > 0) {
+            e = 1;
+            cout << "there was a missing closing tag for : " << IDs[i] << "\n";
+        }
+    }
+    if (e)
+        cout << "IMPORTANT NOTE !!!\nmissing tags may have caused mismatch "
+                "errors\n";
 }
-
 
 /// @brief gets the position of the string in the frequency array
 /// @param s the tag we need to know its frequency
@@ -122,3 +137,36 @@ int get_freq(string s) {
             return i;
     }
 }
+/// @brief corrects mismatched strings
+/// @param xml the cml to correct
+void correct(string &xml) {
+    string s;
+    int pos;
+    if (err.empty()) {
+        cout << "all correct !\n";
+        return;
+    }
+    while (!err.empty()) {
+        s = err.top();
+        pos = positions.back();
+        err.pop();
+        positions.pop_back();
+        cout << "************\n";
+        cout << "case 2 mismatch\n";
+        for (int i = pos - 10; i < xml.size() && i < pos + 10; i++)
+            cout << xml[i];
+        cout << endl << s;
+        cout << "\n************\n";
+        int en = pos;
+        while (xml[en] != '>') {
+            en++;
+        }
+        xml = xml.substr(0, pos + 1) + s + xml.substr(en);
+        cout << "************\n";
+        for (int i = pos - 10; i < xml.size() && i < pos + 10; i++)
+            cout << xml[i];
+        cout << endl << s;
+        cout << "\n************\n\n\n";
+    }
+}
+
--- error_handling/handling.h
+++ error_handling/handling.h
@@ -1,15 +1,18 @@
 #ifndef HANDLING_H
 #define HANDLING_H
-#include "../parsing/parse.h"
-#include <stack>
+
 #include <iostream>
-using namespace std;
+#include <stack>
+#include <string>
 
-void detect(string &xml);
+#include "../parsing/parse.h"
+
+void detect(std::string &xml);
 int get_freq(std::string s);
+void correct(std::string &xml);
 
-stack<string> tags;
-stack<string> err;
-vector<int> positions;
+extern std::stack<std::string> tags;
+extern std::stack<std::string> err;
+extern std::vector<int> positions;
 
 #endif
